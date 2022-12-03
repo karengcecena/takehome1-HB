61bd4a83c6838e406af0ba2d18da575379da83ea
karengcecena <karengcecena@gmail.com>
2022-12-02 18:32:06 -0800

    Basic Login Page Inputs Created

--- server.py
+++ server.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import (Flask, render_template, request, flash, session,
+                   redirect)
+
+from jinja2 import StrictUndefined
 
 
 app = Flask(__name__)
+app.secret_key = "session_secret"
+app.jinja_env.undefined = StrictUndefined
 
+@app.route("/")
+def homepage():
+    """View login page"""
+
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
