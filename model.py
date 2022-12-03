7ba0b82db8c6e0fbfd2588568423e64c9423a26d
karengcecena <karengcecena@gmail.com>
2022-12-02 19:03:27 -0800

    Created User and Appointment tables in DB

--- model.py
+++ model.py
@@ -2,6 +2,43 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    """User information"""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(50), unique=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String())
+    appointment_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
+
+    # middle table: 
+    appointments = db.relationship('Appointment', back_populates="user")
+
+    def __repr__(self):
+        """Show info about User"""
+
+        return f"<User user_id = {self.user_id} username = {self.username} email = {self.email}>"
+
+class Appointment(db.Model):
+    """Appointment information"""
+
+    __tablename__ = "appointments"
+
+    appointment_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    appointment_start_time = db.Column(db.DateTime)
+    appointment_end_time = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.user_id"), nullable=False)
+
+    # middle table: 
+    user = db.relationship('User', back_populates="appointments")
+
+    def __repr__(self):
+        """Show info about the Appointment"""
+
+        return f"<Media appointment_id: {self.appointment_id} appointment_start_time: {self.appointment_start_time} appointment_end_time: {self.appointment_end_time}>"
+
 def connect_to_db(app, db_uri="postgresql:///project_db", echo=True):
     """Connect to database."""
 
